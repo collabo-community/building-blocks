f9e1d825af791747f566660b5976a2e16624d753
Abraham Akpan <akpan@ogunlabs.com>
2024-07-01 06:23:32 +0100

    remove deleting for css since it has been handled

--- watch-delete.js
+++ watch-delete.js
@@ -11,17 +11,11 @@ watcher
   .on('unlink', filePath => {
     const relativePath = path.relative(scssDir, filePath);
     const cssMapPath = path.join(cssDir, relativePath.replace(/\.scss$/, '.css.map'));
-    const cssPath = path.join(cssDir, relativePath.replace(/\.scss$/, '.css'));
 
-    // Delete the corresponding .css and .css.map files
+    // Delete the corresponding .css.map files
     if (fs.existsSync(cssMapPath)) {
       fs.unlinkSync(cssMapPath);
       console.log(`Deleted ${cssMapPath}`);
-    }
-
-    if (fs.existsSync(cssPath)) {
-      fs.unlinkSync(cssPath);
-      console.log(`Deleted ${cssPath}`);
     }
   })
   .on('error', error => console.error(`Watcher error: ${error}`));

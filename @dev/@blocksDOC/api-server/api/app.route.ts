5dbe29fcf9c169e99aba6e2bc39892ac289359f9
Ifycode <marychord@gmail.com>
2024-12-02 23:58:50 +0100

    Fix: Remove blocks doc

--- @dev/@blocksDOC/api-server/api/app.route.ts
+++
@@ -1,8 +0,0 @@
-import express, { IRouter } from 'express';
-import { getAppController } from './app.controller';
-
-const router: IRouter = express.Router();
-
-router.get('/', getAppController);
-
-export { router };
--- @dev/@blocksDOC/api-server/server.ts
+++
@@ -1,8 +0,0 @@
-import { app as app} from './app'
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
